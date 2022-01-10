dfabd6ab2998059ec65de0eb8e018e4582159a1e
Sudip Bajgai <realsudipbajgai@gmail.com>
2022-01-10 11:32:52 +0545

    responsive banner image

--- scripts/swiper_init.js
+++ scripts/swiper_init.js
@@ -6,4 +6,10 @@ var swiper = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
       dynamicBullets: true,
     },
+    breakpoins:{
+      '600':{
+        slidesPerView:1
+      }
+    }
+  
   });
